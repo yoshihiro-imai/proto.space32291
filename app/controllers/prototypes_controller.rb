4d3e4e3f148804102a70e9e0ef685cbbb90081f3
yoshihiro-imai <75416155+yoshihiro-imai@users.noreply.github.com>
2020-12-24 15:52:41 +0900

    イシューの修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -11,9 +11,7 @@ class PrototypesController < ApplicationController
 
 
   def new
-
     @prototype = Prototype.new
-
   end
 
   def create
@@ -61,5 +59,11 @@ class PrototypesController < ApplicationController
 
   def prototype_params
     params.require(:prototype).permit(:title,:catch_copy,:concept,:image).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    unless user_signed_in?
+    redirect_to action: :index
+    end
   end
 end
